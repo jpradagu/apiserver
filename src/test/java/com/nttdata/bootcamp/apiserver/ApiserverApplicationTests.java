ff3d442a37b23e4189e10d63f79fefac1729a61d
Jimmy Prada <jipragu@emeal.nttdata.com>
2022-06-22 15:36:28 -0500

    fix checkstyle.xml

--- src/test/java/com/nttdata/bootcamp/apiserver/ApiserverApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/apiserver/ApiserverApplicationTests.java
@@ -3,11 +3,14 @@ package com.nttdata.bootcamp.apiserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * ApiServerApplicationTests.
+ */
 @SpringBootTest
 class ApiserverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
